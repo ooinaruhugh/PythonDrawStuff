f15fcd72745257622e42be36c5419d021cdd27c5
Kamillo Ferry <git.hugh@gmail.com>
2019-01-24 23:19:37 +0100

    First picture generation step

---
+++ simplepicture.py
@@ -0,0 +1,24 @@
+from PIL import Image
+from itertools import *
+
+width = 100
+height = 100
+pixelcount = width * height
+
+def sampleimage():
+    high = repeat(255, pixelcount//2)
+    low = repeat(0,pixelcount//2)
+    reds = chain.from_iterable([repeat(255, pixelcount//2), repeat(0,pixelcount//2)])
+    blue = chain.from_iterable([repeat(0, pixelcount//2), repeat(255, pixelcount//2)])
+    green = repeat(0, pixelcount)
+    return zip(reds, blue, green)
+
+if __name__ == "__main__":
+    #draw = ImageDraw.Draw(im) # This might be used to create a canvas :)
+
+    im = Image.new("RGB", (width, height), "white");
+    
+    # i.e. map(putpixel, coord, pixel)
+    for coords, pixel in zip(product(range(width), range(height)), sampleimage()):
+        im.putpixel(coords, pixel)
+    im.save("test.png")
