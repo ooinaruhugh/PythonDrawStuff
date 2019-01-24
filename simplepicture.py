bbed72ec8027ad82e863ac9c7d180147b1a5c884
Kamillo Ferry <git.hugh@gmail.com>
2019-01-24 23:20:25 +0100

    Another approach for iterators

--- simplepicture.py
+++ simplepicture.py
@@ -6,19 +6,18 @@ height = 100
 pixelcount = width * height
 
 def sampleimage():
-    high = repeat(255, pixelcount//2)
-    low = repeat(0,pixelcount//2)
-    reds = chain.from_iterable([repeat(255, pixelcount//2), repeat(0,pixelcount//2)])
-    blue = chain.from_iterable([repeat(0, pixelcount//2), repeat(255, pixelcount//2)])
+    high = repeat(255)
+    low = repeat(0)
+    reds = list(islice(high, pixelcount//2)) + list(islice(low, pixelcount//2))
+    blue = list(islice(low, pixelcount//2)) + list(islice(high, pixelcount//2))
     green = repeat(0, pixelcount)
-    return zip(reds, blue, green)
+    return zip(reds, green, blue)
 
 if __name__ == "__main__":
     #draw = ImageDraw.Draw(im) # This might be used to create a canvas :)
 
     im = Image.new("RGB", (width, height), "white");
-    
-    # i.e. map(putpixel, coord, pixel)
+
     for coords, pixel in zip(product(range(width), range(height)), sampleimage()):
         im.putpixel(coords, pixel)
     im.save("test.png")
