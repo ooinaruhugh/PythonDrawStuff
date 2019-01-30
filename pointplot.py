8a3744147cf093cabf7458f804c4ec3c95ecf162
Kamillo Ferry <git.hugh@gmail.com>
2019-01-30 23:24:17 +0100

    Removed unnecessary parameter in ranges

--- pointplot.py
+++ pointplot.py
@@ -1,7 +1,10 @@
 from itertools import product
 
 def plane(width, height):
-    return product(range(0, width), range(0, height))
+    return product(range(width), range(height))
 
-def translate(coordinates, dx, dy):
-    return map(lambda v : (v[0] + dx, v[1] + dy), coordinates)
+def translate_2d(coordinate, dx, dy):
+    return (coordinate[0] + dx, coordinate[1] + dy)
+
+def scale(coordinate, l):
+    return tuple([l * x for x in coordinate])
